3457a216af01eb09ece3e1a33f44620dc00550fb
taueyusuke <etoauve@icloud.com>
2019-09-29 13:00:28 +0900

    usermodelのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "有効なfactoryを持つ" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  it "nameがなければ無効" do
+    user = FactoryBot.build(:user, name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "emailがなければ無効" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "passwordがなければ無効" do
+    user = FactoryBot.build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it "重複したメールアドレスなら無効な状態" do
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 end
