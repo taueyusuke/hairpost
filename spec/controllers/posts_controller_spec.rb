98eaed5542b4aee6eacb7dcf1e6bd21e08b7bbad
taueyusuke <etoauve@icloud.com>
2019-09-30 18:16:53 +0900

    postのリクエストを追加

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   describe "#index" do
-    it "responds successfully" do
+    it "正常にレスポンスを返す" do
       get :index
       expect(response).to be_success
+    end
+
+    it "200レスポンスを返す" do
+      get :index
+      expect(response).to have_http_status "200"
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name "testuser"
-    sequence(:email) { |n| "tester#{n}@example.com" }
+    sequence(:email) { |n| "testuser#{n}@test.com" }
     password "password"
   end
 end
