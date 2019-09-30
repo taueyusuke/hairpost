fac0b473491a00f9239444e37af2f0230b9a29e0
taueyusuke <etoauve@icloud.com>
2019-09-30 10:29:08 +0900

    usermodelの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
-  validates :caption, presence: true
-
   belongs_to :user
 
   has_many :photos, dependent: :destroy
@@ -9,7 +7,19 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  has_many :likes, dependent: :destroy
+
   accepts_nested_attributes_for :photos
+
+  validates :caption, presence: true
+
+  validates :sex, presence: true
+
+  validates :length, presence: true
+
+  validates :haircolor, presence: true
+
+  validates :hairmemu, presence: true
 
   def liked_by(user)
     Like.find_by(user_id: user.id, post_id: id)
