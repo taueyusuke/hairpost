25db9305dd794d214979db8aa381e5595098987b
taueyusuke <etoauve@icloud.com>
2019-09-30 10:21:58 +0900

    usershowアクションの編集

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
+  protect_from_forgery except: %i[new create]
 
   protected
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
+    @posts = Post.page(params[:page])
+                 .where(user_id: @user.id)
+                 .order('created_at desc')
+                 .includes(:user, :photos)
+  end
   end
 end
