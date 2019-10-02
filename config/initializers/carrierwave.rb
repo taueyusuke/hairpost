18cc5b121d4acb48629874793399ea0d78ea461d
taueyusuke <etoauve@icloud.com>
2019-10-02 23:30:18 +0900

    first commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
         provider: 'AWS',
-        aws_access_key_id: ENV['AKIAS6JWV4IFFCGOAA66'],
-        aws_secret_access_key: ENV['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'],
+        aws_access_key_id: ENV[AKIAS6JWV4IFFCGOAA66],
+        aws_secret_access_key: ENV[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],
         region: 'ap-northeast-1'
     }
     config.fog_directory  = ENV['hairsnspost']
